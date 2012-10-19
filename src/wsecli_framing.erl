3caf6eed3deaa1d76b02adffa75185d9e8a290f6
Jin Chen <jinchen@UrTurn-build-dev.eac.ad.ea.com>
2012-10-19 07:43:36 -0700

    typo again for change back framing

--- src/wsecli_framing.erl
+++ src/wsecli_framing.erl
@@ -61,13 +61,13 @@ from_binary(Data) ->
 %  {<<>>, lists:reverse(Acc)};
 
 from_binary(<<Head:9, 126:7, PayloadLen:16, Payload:PayloadLen/binary, Rest/binary>>, Acc)->
-  from_binary(Rest, [decode_frame(<<Head, 126:7, PayloadLen, Payload>>) | Acc]);
+  from_binary(Rest, [decode_frame(<<Head, 126:7, PayloadLen, Payload/binary>>) | Acc]);
 
 from_binary(<<Head:9, 127:7, PayloadLen:64, Payload:PayloadLen/binary, Rest/binary>>, Acc)->
-  from_binary(Rest, [decode_frame(<<Head, 127:7, PayloadLen, Payload>>) | Acc]);
+  from_binary(Rest, [decode_frame(<<Head, 127:7, PayloadLen, Payload/binary>>) | Acc]);
 
 from_binary(<<Head:9, PayloadLen:7, Payload:PayloadLen/binary, Rest/binary>>, Acc) ->
-  from_binary(Rest, [decode_frame(<<Head, PayloadLen, Payload>>) | Acc]);
+  from_binary(Rest, [decode_frame(<<Head, PayloadLen, Payload/binary>>) | Acc]);
 
 from_binary(Bin, Acc) when is_binary(Bin) ->
   {Bin, lists:reverse(Acc)}.
