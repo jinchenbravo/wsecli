593f4c8f21006a6d22a75932cda8e99044a60069
Jin Chen <jinchen@UrTurn-build-dev.eac.ad.ea.com>
2012-10-22 11:10:07 -0700

    new version of wescli_message

--- src/wsecli_message.erl
+++ src/wsecli_message.erl
@@ -35,8 +35,8 @@ decode(Data) ->
   decode(Data, begin_message, #message{}).
 
 -spec decode(Data::binary(), Message::#message{}) -> list(#message{}).
-decode(Data, Message) ->
-  decode(Data, continue_message, Message).
+ decode(Data, Message) ->
+   decode(Data, continue_message, Message).
 
 
 %
@@ -71,46 +71,67 @@ frame(Data, Options) ->
 -spec decode(Data::binary(), Type :: message_type(), Message::#message{}) -> list(#message{}).
 decode(Data, begin_message, _Message) ->
   {Bin, Frames} = wsecli_framing:from_binary(Data),
-  {Bin, lists:reverse(process_frames(begin_message, Frames, []))};
+  {Bin, lists:reverse(process_frames(Frames, [], []))};
+  % {Bin, lists:reverse(process_frames(begin_message, Frames, []))};
 
 decode(Data, continue_message, Message) ->
-  {Bin, Frames} = wsecli_framing:from_binary(Data),
-  {Bin, lists:reverse(process_frames(continue_message, Frames, [Message | []]))}.
+   {Bin, Frames} = wsecli_framing:from_binary(Data),
+   {Bin, lists:reverse(process_frames(Frames, Message#message.frames, []))}.
 
--spec process_frames(Type:: message_type(), Frames :: list(#frame{}), Messages :: list(#message{})) -> list(#message{}).
-process_frames(_, [], Acc) ->
-  Acc;
-process_frames(begin_message, Frames, Acc) ->
-  wtf(Frames, #message{}, Acc);
+-spec process_frames(Frames::list(#frame{}), FrameAcc::list(#frame{}), MessageAcc::list(#message{})) -> list(#message{}).
 
-process_frames(continue_message, Frames, [FramgmentedMessage | Acc]) ->
-  wtf(Frames, FramgmentedMessage, Acc).
+process_frames([], [], Macc) ->
+  Macc;
 
-wtf([], _, Acc) ->
-  Acc;
-  
-wtf([#frame{ payload = <<>> } | Frames], XMessage, Acc) ->
-  %% skip this
-  wtf(Frames, XMessage, Acc);
+process_frames([], Facc, Macc) ->
+  Msg = bm(Facc,fragmented),
+  [Msg|Macc];
 
-wtf([Frame | Frames], XMessage, Acc) ->
-  case process_frame(Frame, XMessage) of
-    {fragmented, Message} ->
-      process_frames(continue_message, Frames, [Message#message{type = fragmented} | Acc]);
-    {completed, Message} ->
-      process_frames(begin_message, Frames, [Message | Acc])
-  end.
-
--spec process_frame(Frame :: #frame{}, Message :: #message{})-> {fragmented | completed, #message{}}.
-process_frame(Frame, Message) ->
+process_frames([Frame|Frames], Facc, Macc) ->
   case contextualize_frame(Frame) of
-    continue ->
-      Frames = Message#message.frames,
-      {fragmented, Message#message{frames = [Frame | Frames]}};
     close ->
-      BuiltMessage = build_message(Message, lists:reverse([Frame | Message#message.frames])),
-      {completed, BuiltMessage}
+      NewFacc = [Frame|Facc],
+      Msg = bm(NewFacc,completed),
+      NewMacc = [Msg|Macc],
+      process_frames(Frames, [], NewMacc);
+    continue ->
+      NewFacc = [Frame|Facc],
+      process_frames(Frames,NewFacc,Macc)
   end.
+% -spec process_frames(Type:: message_type(), Frames :: list(#frame{}), Messages :: list(#message{})) -> list(#message{}).
+% process_frames(_, [], Acc) ->
+%   Acc;
+% process_frames(begin_message, Frames, Acc) ->
+%   wtf(Frames, #message{}, Acc);
+
+% process_frames(continue_message, Frames, [FramgmentedMessage | Acc]) ->
+%   wtf(Frames, FramgmentedMessage, Acc).
+
+% wtf([], _, Acc) ->
+%   Acc;
+  
+% wtf([#frame{ payload = <<>> } | Frames], XMessage, Acc) ->
+%   %% skip this
+%   wtf(Frames, XMessage, Acc);
+
+% wtf([Frame | Frames], XMessage, Acc) ->
+%   case process_frame(Frame, XMessage) of
+%     {fragmented, Message} ->
+%       process_frames(continue_message, Frames, [Message#message{type = fragmented} | Acc]);
+%     {completed, Message} ->
+%       process_frames(begin_message, Frames, [Message | Acc])
+%   end.
+
+% -spec process_frame(Frame :: #frame{}, Message :: #message{})-> {fragmented | completed, #message{}}.
+% process_frame(Frame, Message) ->
+%   case contextualize_frame(Frame) of
+%     continue ->
+%       Frames = Message#message.frames,
+%       {fragmented, Message#message{frames = [Frame | Frames]}};
+%     close ->
+%       BuiltMessage = build_message(Message, lists:reverse([Frame | Message#message.frames])),
+%       {completed, BuiltMessage}
+%   end.
 
 -spec contextualize_frame(Frame :: #frame{}) -> close | continue.
 contextualize_frame(Frame) ->
@@ -119,25 +140,34 @@ contextualize_frame(Frame) ->
     {0, _} -> continue
   end.
 
+-spec bm(Frames :: list(#frame{}), fragmented|completed) -> #message{}.
+bm(Frames,MsgType) ->
+  case MsgType of
+    fragmented ->
+      #message{frames = Frames, type = fragmented};
+    completed ->
+      build_message(#message{},Frames)
+  end.
+
 build_message(Message, Frames) ->
   [HeadFrame | _] = Frames,
 
   case HeadFrame#frame.opcode of
     1 ->
       Payload = build_payload_from_frames(text, Frames),
-      Message#message{type = text, payload = Payload};
+      Message#message{frames = Frames, type = text, payload = Payload};
     2 ->
       Payload = build_payload_from_frames(binary, Frames),
-      Message#message{type = binary, payload = Payload};
+      Message#message{frames = Frames, type = binary, payload = Payload};
     8 ->
       Payload = build_payload_from_frames(close, Frames),
-      Message#message{type = close, payload = Payload};
+      Message#message{frames = Frames, type = close, payload = Payload};
     9 ->
       Payload = build_payload_from_frames(text, Frames),
-      Message#message{type = ping, payload = Payload};
+      Message#message{frames = Frames, type = ping, payload = Payload};
     10 ->
       Payload = build_payload_from_frames(text, Frames),
-      Message#message{type = pong, payload = Payload}
+      Message#message{frames = Frames, type = pong, payload = Payload}
   end.
 
 build_payload_from_frames(close, [Frame]) ->
