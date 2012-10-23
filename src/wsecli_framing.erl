f2e1e889b491dcad739593f5c85f1d72aba51979
Jin Chen <jinchen@UrTurn-build-dev.eac.ad.ea.com>
2012-10-23 07:42:31 -0700

    fix framing again Rest1 and Rest2

--- src/wsecli_framing.erl
+++ src/wsecli_framing.erl
@@ -60,19 +60,19 @@ from_binary(Data) ->
 from_binary(<<Head:9, 126:7, PayloadLen:16/binary, Payload:PayloadLen/binary, Rest/binary>>, Acc)->
   lager:info ("inside from_binary and PayloadLen is ~p",[PayloadLen]),
   lager:info ("inside from_binary and Payload is ~p",[Payload]),
-  Rest1 = <<Payload,Rest>>,
+  Rest1 = <<Payload/binary,Rest/binary>>,
   PL = PayloadLen * 8,
   <<PLD:PL/binary, Rest2/binary>> = Rest1,
   from_binary(Rest2, [decode_frame(<<Head:9/binary, 126:7, PayloadLen:16/binary, PLD:PL/binary>>) | Acc]);
 
 from_binary(<<Head:9, 127:7, PayloadLen:64, Payload:PayloadLen/binary, Rest/binary>>, Acc)->
-  Rest1 = <<Payload,Rest>>,
+  Rest1 = <<Payload/binary,Rest/binary>>,
   PL = PayloadLen * 8,
   <<PLD:PL/binary, Rest2/binary>> = Rest1,
   from_binary(Rest2, [decode_frame(<<Head:9/binary, 127:7, PayloadLen:64/binary, PLD:PL/binary>>) | Acc]);
 
 from_binary(<<Head:9, PayloadLen:7, Payload:PayloadLen/binary, Rest/binary>>, Acc) ->
-  Rest1 = <<Payload,Rest>>,
+  Rest1 = <<Payload/binary,Rest/binary>>,
   PL = PayloadLen * 8,
   <<PLD:PL/binary, Rest2/binary>> = Rest1,
   from_binary(Rest2, [decode_frame(<<Head:9/binary, PayloadLen:7/binary, PLD:PL/binary>>) | Acc]);
