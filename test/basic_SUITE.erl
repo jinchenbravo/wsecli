723cd43a3ab749da779534cb56515827457532c8
Jin Chen <jinchenbravo@gmail.com>
2013-03-07 06:53:09 -0800

    adding test cases for payloadlen is bigger than the actual lenth or the other way around

--- test/basic_SUITE.erl
+++ test/basic_SUITE.erl
@@ -36,7 +36,32 @@ decode_test(_Config)->
 	B3 = <<136,0>>,
 	{RB2,WM2} = wsecli_message:decode(B3),
 	io:format("WM2 is ~p",[WM2]),	
-	M2 = WM2
+	M2 = WM2,
+
+	%
+	% if the actual binary length is longer than the payloadlen in the header, then after make frame(s) the remaining binary
+	% needs to be returned.
+	% In this case, becasue the second byte is 126, so according to RFC 6455 the next 16 bits will be the payloadlen, and it 
+	% has the value of 130 then the last byte 131 has to be returned as a remianing binary
+	%
+	B4 = <<130,126,0,130,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,
+	37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,
+	77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,
+	113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131>>,
+	{RB4,WM4} = wsecli_message:decode(B4),
+	io:format("WM4 is ~p",[WM4]),
+	io:format("RB4 is ~p",[RB4]),
+	RB4 = <<131>>,
+
+	%
+	% if the payloadlen is 126, but the actual binary length is smaller, which means it's the first part of one frame.
+	% then the whole thing has to be returned as a remaining binary.
+	%
+	B5 = <<130,126,1,2,3,4,5>>,
+	{RB5,WM5} = wsecli_message:decode(B5),
+	io:format("WM5 is ~p",[WM5]),
+	io:format("RB5 is ~p",[RB5]),
+	RB5 = B5
 	.
 
 encode_test(_Config)->
@@ -53,5 +78,12 @@ encode_test(_Config)->
 	B2 = wsecli_message:encode(<<>>,close),
 	io:format("B2 is ~p",[B2]),
 	P2 = [<<136,0>>],
-	B2 = P2		
+	B2 = P2,
+
+	B3 = wsecli_message:encode("a",text),
+	io:format("B3 is ~p",[B3]),
+	% {RB3,WM3} = wsecli_message:decode(B3),
+	% io:format("WM3 is ~p",[WM3]),
+	B4 = wsecli_message:encode("b",text),
+	io:format("B4 is ~p",[B4])	
 	.
